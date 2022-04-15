8a637d46dfbea1f2894e2b981ed4af6d5d99ea58
mangesh9903 <mangeshb9903@gmail.com>
2022-04-15 19:33:08 +0530

    Welcome to Cab Invoice Generator System.

---
+++ src/main/java/com/bridgelabz/App.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println("Welcome to Cab Invoice Generator System.");
+    }
+}
